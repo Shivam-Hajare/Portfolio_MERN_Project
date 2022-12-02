e26b8ad09e0f126c6f6f2cd4f0e3d2a65afd4f57
Shivam Hajare <shivamhajare96@gmail.com>
2022-12-02 11:12:29 +0530

    DB_cconnection&secure_mongoURL

--- server/app.js
+++ server/app.js
@@ -1,10 +1,36 @@
+const dotenv = require("dotenv")
+
 const express = require("express")
+const app = express();
 
-const app=express();
 
-app.get("/",(req,res)=>{
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+dotenv.config({ path: "./config.env" })
+
+const PORT = process.env.PORT
+
+
+require("./db/connection")
+
+
+app.get("/", (req, res) => {
     res.send("hello world")
 })
-app.listen(3000,()=>{
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
     console.log("server is running.....");
 })
